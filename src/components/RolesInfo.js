1e932410d0d28e6e93b54784d5861b51db55ee81
niravparikh05 <nir.parikh05@gmail.com>
2022-05-23 12:51:23 +0530

    added initial changes for namespace support

--- src/components/RolesInfo.js
+++ src/components/RolesInfo.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { RoleTypes } from "constants/RoleTypes";
 
 function RolesInfo({ roles, projectId, addGroupInRole = null }) {
+  const [namespacesList, setNamespacesList] = useState([]);
+
+  const getNamespaceNames = () => {
+    if (!roles?.length) return [];
+
+    const selectedNamespaces = roles?.reduce((acc, curr) => {
+      const { role, namespace_id } = curr;
+      if (role?.name?.includes("NAMESPACE") && namespace_id) {
+        const namespace = thisProjectNamespaces?.find(
+          pn => pn.metadata?.id === namespace_id
+        );
+        if (namespace) acc.push(namespace.metadata.name);
+      }
+      return acc;
+    }, []);
+    const uniqueNamespace = [...new Set(selectedNamespaces)];
+    return uniqueNamespace;
+  };
+
+  useEffect(() => {
+    const dataRoles = roles?.map(r => r?.role?.name);
+    const uniqueDataRoles = [...new Set(dataRoles)];
+    const isNamespaceRole = uniqueDataRoles?.join().includes("NAMESPACE");
+
+    if (isNamespaceRole) setNamespacesList(getNamespaceNames());
+  }, [roles]);
+
   const getRoleStrings = () => {
     const parsedRoles = roles.map((role) => {
       let roleString = RoleTypes[role];
