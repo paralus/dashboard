77c6934ce9f4f467e30e722e3e221cba80f659ac
Abin Simon <mail@meain.io>
2022-06-08 18:52:50 +0530

    Use human friendly date format

--- src/components/DateFormat.js
+++ src/components/DateFormat.js
@@ -1,11 +1,19 @@
-import React from "react";
+const dateFormatOptions = {
+  year: "numeric",
+  month: "2-digit",
+  day: "2-digit",
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
+  timeZoneName: "short",
+};
 
 const DateFormat = ({ timestamp }) => {
   if (!timestamp) return "-";
   try {
-    return new Date(
-      timestamp.seconds * 1000 + timestamp.nanos / 1e6
-    ).toISOString();
+    return new Intl.DateTimeFormat("en-US", dateFormatOptions).format(
+      new Date(timestamp.seconds * 1000 + timestamp.nanos / 1e6)
+    );
   } catch (error) {
     return "-";
   }
