a382f85da072912b933c62894a2e929aa3e37877
Abin Simon <mail@meain.io>
2022-07-28 12:28:27 +0530

    Fix timestamp rendering for audit logs

--- src/components/DateFormat.js
+++ src/components/DateFormat.js
@@ -11,9 +11,19 @@ const dateFormatOptions = {
 const DateFormat = ({ timestamp }) => {
   if (!timestamp) return "-";
   try {
-    return new Intl.DateTimeFormat("en-US", dateFormatOptions).format(
-      new Date(timestamp.seconds * 1000 + timestamp.nanos / 1e6)
-    );
+    if (typeof timestamp === "number") {
+      return new Intl.DateTimeFormat("en-US", dateFormatOptions).format(
+        timestamp
+      );
+    } else if (typeof timestamp === "string") {
+      return new Intl.DateTimeFormat("en-US", dateFormatOptions).format(
+        Date.parse(timestamp)
+      );
+    } else {
+      return new Intl.DateTimeFormat("en-US", dateFormatOptions).format(
+        new Date(timestamp.seconds * 1000 + timestamp.nanos / 1e6)
+      );
+    }
   } catch (error) {
     return "-";
   }
